38e1907aca3ab417e5b13593e25bab86380c43b6
Your Name <you@example.com>
2018-09-18 22:04:14 +0200

    gitlab

--- gitlab/gitlab-start.sh
+++ gitlab/gitlab-start.sh
@@ -6,6 +6,9 @@ docker service create --name gitlab \
   --mount type=bind,source="/var/run/docker.sock",destination="/var/run/docker.sock" \
   --publish mode=ingress,target=22,published=222 \
   --network private \
+  --label "traefik.enable=true" \
+  --label "traefik.port=80" \
+  --label "traefik.frontend.rule=Host:git.example.com" \
 gitlab/gitlab-ce:latest
 
 docker service create --name gitlab-runner \
