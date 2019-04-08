543ceb8e9bfac0a1284749a01830e01479eb7641
port22 <github@port22.it>
2019-04-08 16:09:52 +0200

    Create run.sh

---
+++ squid/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+IMAGE=""
+SERVICE="squid"
+
+docker service rm $SERVICE
+
+docker service create --with-registry-auth --detach --name $SERVICE \
+  --constraint 'node.role == manager' \
+  --mount type=bind,source=$PWD/squid.conf,destination=/etc/squid/squid.conf \
+  --mount type=bind,source=$PWD/auth,destination=/auth \
+  --publish mode=ingress,published=3128,target=3128 \
+$IMAGE
